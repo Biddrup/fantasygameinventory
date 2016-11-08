0bd7b693b4311386e0146de2da661d16387c3358
Biddrup Kumar Mallick <Biddrup@users.noreply.github.com>
2016-11-09 00:30:27 +0600

    Update fantasygameinventory.py
    Trying to solve :-)

--- fantasygameinventory.py
+++ fantasygameinventory.py
@@ -1,4 +1,20 @@
 # inventory.py
+
+def displayInventory(inventory):
+    print("Inventory:")
+    item_total = 0
+    for k, v in inventory.items():
+        print(str(v) + ' ' + str(k))
+        item_total += v
+    print("Total number of items: " + str(item_total))
+
+def addToInventory(inventory, addedItems):
+    # your code goes here
+    for i in range(len(addedItems)):
+        inventory.setdefault(addedItems[i], 0)
+        inventory[addedItems[i]] = inventory[addedItems[i]] + 1
+    return inventory
+
 #This is take a stuff from you
 stuff = {}
 
@@ -32,22 +48,8 @@ while True:
         print('Stuff database updated')
 
 
-def displayInventory(inventory):
-    print("Inventory:")
-    item_total = 0
-    for k, v in inventory.items():
-        print(str(v) + ' ' + str(k))
-        item_total += v
-    print("Total number of items: " + str(item_total))
-
 displayInventory(stuff)
 
-def addToInventory(inventory, addedItems):
-    # your code goes here
-    for i in range(len(addedItems)):
-        inventory.setdefault(addedItems[i], 0)
-        inventory[addedItems[i]] = inventory[addedItems[i]] + 1
-    return inventory
 
 inv = {'gold coin': 42, 'rope': 1}
 dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
